1ba85c06a2038923cc79fcd5232e1cf29034c379
herobaku <mammedovemin@gmail.com>
2024-02-20 15:34:05 +0400

    add info into section testimonials

--- src/home/Page.jsx
+++ src/home/Page.jsx
@@ -175,7 +175,132 @@ const Home = () => {
               <img src="../../public/image.jpeg" alt="web" />
             </div>
             <div className={styles.course__cards_bottom}>
-              <div className={styles.course__cards_group}>
+              <div className={styles.course__cards_info}>
+                <div className={styles.course__cards_date}>
+                  <button>4 Weeks</button>
+                  <button>Beginner</button>
+                </div>
+                <div className={styles.course__cards_author}>
+                  <h4>By John Smith</h4>
+                </div>
+              </div>
+              <div className={styles.course__cards_web}>
+                <h2>Web Design Fundamentals</h2>
+                <p>
+                  Learn the fundamentals of web design, including HTML, CSS, and
+                  responsive design principles. Develop the skills to create
+                  visually appealing and user-friendly websites.
+                </p>
+              </div>
+              <button className={styles.course__cards_get}>Get it Now</button>
+            </div>
+          </div>
+          <div className={styles.course__cards_item}>
+            <div className={styles.course__cards_top}>
+              <img src="../../public/image.jpeg" alt="web" />
+            </div>
+            <div className={styles.course__cards_bottom}>
+              <div className={styles.course__cards_info}>
+                <div className={styles.course__cards_date}>
+                  <button>4 Weeks</button>
+                  <button>Beginner</button>
+                </div>
+                <div className={styles.course__cards_author}>
+                  <h4>By John Smith</h4>
+                </div>
+              </div>
+              <div className={styles.course__cards_web}>
+                <h2>Web Design Fundamentals</h2>
+                <p>
+                  Learn the fundamentals of web design, including HTML, CSS, and
+                  responsive design principles. Develop the skills to create
+                  visually appealing and user-friendly websites.
+                </p>
+              </div>
+              <button className={styles.course__cards_get}>Get it Now</button>
+            </div>
+          </div>
+          <div className={styles.course__cards_item}>
+            <div className={styles.course__cards_top}>
+              <img src="../../public/image.jpeg" alt="web" />
+            </div>
+            <div className={styles.course__cards_bottom}>
+              <div className={styles.course__cards_info}>
+                <div className={styles.course__cards_date}>
+                  <button>4 Weeks</button>
+                  <button>Beginner</button>
+                </div>
+                <div className={styles.course__cards_author}>
+                  <h4>By John Smith</h4>
+                </div>
+              </div>
+              <div className={styles.course__cards_web}>
+                <h2>Web Design Fundamentals</h2>
+                <p>
+                  Learn the fundamentals of web design, including HTML, CSS, and
+                  responsive design principles. Develop the skills to create
+                  visually appealing and user-friendly websites.
+                </p>
+              </div>
+              <button className={styles.course__cards_get}>Get it Now</button>
+            </div>
+          </div>
+          <div className={styles.course__cards_item}>
+            <div className={styles.course__cards_top}>
+              <img src="../../public/image.jpeg" alt="web" />
+            </div>
+            <div className={styles.course__cards_bottom}>
+              <div className={styles.course__cards_info}>
+                <div className={styles.course__cards_date}>
+                  <button>4 Weeks</button>
+                  <button>Beginner</button>
+                </div>
+                <div className={styles.course__cards_author}>
+                  <h4>By John Smith</h4>
+                </div>
+              </div>
+              <div className={styles.course__cards_web}>
+                <h2>Web Design Fundamentals</h2>
+                <p>
+                  Learn the fundamentals of web design, including HTML, CSS, and
+                  responsive design principles. Develop the skills to create
+                  visually appealing and user-friendly websites.
+                </p>
+              </div>
+              <button className={styles.course__cards_get}>Get it Now</button>
+            </div>
+          </div>
+          <div className={styles.course__cards_item}>
+            <div className={styles.course__cards_top}>
+              <img src="../../public/image.jpeg" alt="web" />
+            </div>
+            <div className={styles.course__cards_bottom}>
+              <div className={styles.course__cards_info}>
+                <div className={styles.course__cards_date}>
+                  <button>4 Weeks</button>
+                  <button>Beginner</button>
+                </div>
+                <div className={styles.course__cards_author}>
+                  <h4>By John Smith</h4>
+                </div>
+              </div>
+              <div className={styles.course__cards_web}>
+                <h2>Web Design Fundamentals</h2>
+                <p>
+                  Learn the fundamentals of web design, including HTML, CSS, and
+                  responsive design principles. Develop the skills to create
+                  visually appealing and user-friendly websites.
+                </p>
+              </div>
+              <button className={styles.course__cards_get}>Get it Now</button>
+            </div>
+          </div>
+          <div className={styles.course__cards_item}>
+            <div className={styles.course__cards_top}>
+              <img src="../../public/image.jpeg" alt="web" />
+            </div>
+            <div className={styles.course__cards_bottom}>
+              <div className={styles.course__cards_info}>
                 <div className={styles.course__cards_date}>
                   <button>4 Weeks</button>
                   <button>Beginner</button>
@@ -197,7 +322,17 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className={styles.testimonials}></section>
+      <section className={styles.testimonials}>
+      <h2>Our Testimonials</h2>
+        <div className={styles.benefits__group}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget
+            elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum
+            eget habitasse in velit fringilla feugiat senectus in.
+          </p>
+          <button className={styles.button__view}>View All</button>
+        </div>
+      </section>
       <section className={styles.pricing}></section>
       <section className={styles.accordion}></section>
     </div>
